3f7d94b37d8d105faa6bc8cfd6b211bec1401e32
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-04-07 22:31:38 +0700

    create core package

---
+++ app/src/main/java/com/mikirinkode/kotakfilm/core/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.mikirinkode.kotakfilm.core.utils
+
+class Constants {
+    companion object{
+        const val BASE_URL = "https://api.themoviedb.org/3/"
+        const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
+        const val API_KEY = ""
+    }
+}
