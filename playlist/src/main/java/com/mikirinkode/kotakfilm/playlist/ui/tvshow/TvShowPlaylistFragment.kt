3c3b8d6aa1ac200c91607b13f203fef1f37ce3f4
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-04-13 16:53:51 +0700

    implement dynamic feature

--- playlist/src/main/java/com/mikirinkode/kotakfilm/playlist/ui/tvshow/TvShowPlaylistFragment.kt
+++ playlist/src/main/java/com/mikirinkode/kotakfilm/playlist/ui/tvshow/TvShowPlaylistFragment.kt
@@ -1,5 +1,6 @@
-package com.mikirinkode.kotakfilm.ui.main.playlist.tvshow
+package com.mikirinkode.kotakfilm.playlist.ui.tvshow
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,24 +9,47 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.mikirinkode.kotakfilm.databinding.FragmentTvShowPlaylistBinding
 import com.mikirinkode.kotakfilm.core.ui.CatalogueAdapter
+import com.mikirinkode.kotakfilm.di.PlaylistModuleDependencies
+import com.mikirinkode.kotakfilm.playlist.ui.PlaylistViewModel
+import com.mikirinkode.kotakfilm.playlist.databinding.FragmentTvShowPlaylistBinding
+import com.mikirinkode.kotakfilm.playlist.di.DaggerPlaylistComponent
+import com.mikirinkode.kotakfilm.playlist.di.ViewModelFactory
 import com.mikirinkode.kotakfilm.ui.detail.DetailCatalogueActivity
-import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.EntryPointAccessors
+import javax.inject.Inject
 
 
-@AndroidEntryPoint
 class TvShowPlaylistFragment : Fragment() {
 
     private var _binding: FragmentTvShowPlaylistBinding? = null
     private val binding get() = _binding!!
     private val tvShowAdapter = CatalogueAdapter()
-    private val playlistViewModel: TvShowPlaylistViewModel by viewModels()
+
+    @Inject
+    lateinit var factory: ViewModelFactory
+
+    private val playlistViewModel: PlaylistViewModel by viewModels { factory }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        DaggerPlaylistComponent.builder()
+            .context(requireContext())
+            .appDependencies(
+                EntryPointAccessors.fromApplication(
+                    activity?.applicationContext as Context,
+                    PlaylistModuleDependencies::class.java
+                )
+            )
+            .build()
+            .inject(this)
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
         _binding = FragmentTvShowPlaylistBinding.inflate(inflater, container, false)
         return binding.root
     }
