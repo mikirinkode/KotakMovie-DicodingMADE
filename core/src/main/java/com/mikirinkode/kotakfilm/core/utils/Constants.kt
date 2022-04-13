efce709035770dfc18bc8517472929d593d26c71
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-04-13 16:55:24 +0700

    move api key to build config file

--- core/src/main/java/com/mikirinkode/kotakfilm/core/utils/Constants.kt
+++ core/src/main/java/com/mikirinkode/kotakfilm/core/utils/Constants.kt
@@ -1,9 +1,11 @@
 package com.mikirinkode.kotakfilm.core.utils
 
+import com.mikirinkode.kotakfilm.core.BuildConfig
+
 class Constants {
     companion object{
         const val BASE_URL = "https://api.themoviedb.org/3/"
         const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
-        const val API_KEY = " "
+        const val API_KEY = BuildConfig.TMDB_API_KEY
     }
 }
