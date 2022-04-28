e4f62627bb8a5ade05d5e5f9e883f5f11017e937
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-04-28 14:07:05 +0700

    move back & change some private field from local properties for a sample

--- core/src/main/java/com/mikirinkode/kotakfilm/core/utils/Constants.kt
+++ core/src/main/java/com/mikirinkode/kotakfilm/core/utils/Constants.kt
@@ -4,14 +4,20 @@ import com.mikirinkode.kotakfilm.core.BuildConfig
 
 class Constants {
     companion object{
-        const val HOSTNAME = BuildConfig.HOSTNAME
-        const val BASE_URL = BuildConfig.BASE_URL
-        const val IMAGE_BASE_URL = BuildConfig.IMAGE_BASE_URL
-        const val API_KEY = BuildConfig.API_KEY
-        const val DB_NAME = BuildConfig.DB_NAME
-        const val PASSPHRASE = BuildConfig.PASSPHRASE
-        const val CERTIFICATE_PIN_1 = BuildConfig.CERTIFICATE_PIN_1
-        const val CERTIFICATE_PIN_2 = BuildConfig.CERTIFICATE_PIN_2
-        const val CERTIFICATE_PIN_3 = BuildConfig.CERTIFICATE_PIN_3
+        const val HOSTNAME = "api.themoviedb.org"
+        const val BASE_URL = "https://api.themoviedb.org/3/"
+        const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
+
+        /**
+         * Get the API Key from
+         * https://developers.themoviedb.org/3
+         */
+        const val API_KEY = "Change this with your own api key"
+
+        const val DB_NAME = "sample_db_name"
+        const val PASSPHRASE = "mikirinkode"
+        const val CERTIFICATE_PIN_1 = "sha256/oD/WAoRPvbez1Y2dfYfuo4yujAcYHXdv1Ivb2v2MOKk="
+        const val CERTIFICATE_PIN_2 = "sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA="
+        const val CERTIFICATE_PIN_3 = "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI="
     }
 }
